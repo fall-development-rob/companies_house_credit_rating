4e9dfab556aa090415c265dcc97bc96340a2015d
fall-development-rob <fall.development@gmail.com>
2025-01-19 08:37:55 +0000

    feat: add custom error handling
    Add some better database error handling

---
+++ src/main/java/com/creditchecker/demo/exception/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.creditchecker.demo.exception;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
